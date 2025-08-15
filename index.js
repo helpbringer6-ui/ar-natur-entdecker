5849cf371acd2a25aee80ae10322669ddd8304e1
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-08-14 23:57:24 -0500

    Add comments to clarify code functionality

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const port = 3000;
 
 // Set up a simple route that returns a welcome message
 app.get('/', (req, res) => {
-  res.send('Willkommen beim AR Natur Entdecker!');
+  res.send('Willkommen beim AR Natur Entdecker!'); // Sends a welcome message to the client
 });
 
 // Start the server and listen on the specified port
 app.listen(port, () => {
-  console.log(`Beispiel-App lauscht auf http://localhost:${port}`);
+  console.log(`Beispiel-App lauscht auf http://localhost:${port}`); // Logs the URL the server is listening on
 });
