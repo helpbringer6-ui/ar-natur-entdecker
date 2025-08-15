07964ea21a401fc6e2bdc448f8d17b5ae5ee00e1
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-08-14 23:44:56 -0500

    Add comment to explain server setup

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Set up a simple route that returns a welcome message
 app.get('/', (req, res) => {
   res.send('Willkommen beim AR Natur Entdecker!');
 });
 
+// Start the server and listen on the specified port
 app.listen(port, () => {
   console.log(`Beispiel-App lauscht auf http://localhost:${port}`);
 });
